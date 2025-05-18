e3942bea38c736608522211af9e2262c31794086
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2025-05-18 11:36:31 +0200

    fix 100% code coverage

--- tests/TagEngine/TagEngineTest.php
+++ tests/TagEngine/TagEngineTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace LordSimal\CustomHtmlElements\Test\TagEngine;
 
+use LordSimal\CustomHtmlElements\Error\RegexException;
 use LordSimal\CustomHtmlElements\TagEngine;
 use PHPUnit\Framework\TestCase;
 
@@ -22,6 +23,7 @@ class TagEngineTest extends TestCase
             'tag_directories' => [
                 dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Tags' . DIRECTORY_SEPARATOR,
                 dirname(__DIR__) . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR,
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Tags' . DIRECTORY_SEPARATOR,
             ],
         ];
         $instance = TagEngine::getInstance($options);
@@ -29,5 +31,33 @@ class TagEngineTest extends TestCase
 
         $sameInstance = TagEngine::getInstance();
         $this->assertSame($instance, $sameInstance);
+    }
+
+    public function testThrowsRegexException(): void
+    {
+        $options = [
+            'tag_directories' => [
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Tags' . DIRECTORY_SEPARATOR,
+                dirname(__DIR__) . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR,
+            ],
+        ];
+        $instance = TagEngine::getInstance($options);
+
+        $test = '';
+        $pcreLimit = 100000;
+        for($i = 0; $i < $pcreLimit; $i++) {
+            $test .= '<c-tag>Test';
+        }
+        for($i = 0; $i < $pcreLimit; $i++) {
+            $test .= '</c-tag>';
+        }
+
+        try {
+            $instance->parse($test);
+        } catch (RegexException $e) {
+            $this->assertEquals('Backtrack limit was exhausted', $e->getMessage());
+            $this->assertNotEmpty($e->getRegex());
+            $this->assertNotEmpty($e->getSource());
+        }
     }
 }
